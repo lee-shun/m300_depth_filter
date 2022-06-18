8f7888546d4ba4d1b7be480007d757e111ec2415
lee-shun <lee970802@163.com>
2022-06-18 16:31:11 -0400

    u

---
+++ M300_trajectory_drawer/local_pose_summary.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+*   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+*
+*   @Filename: local_pose_summary.cpp
+*
+*   @Author: Shun Li
+*
+*   @Email: 2015097272@qq.com
+*
+*   @Date: 2022-06-18
+*
+*   @Description: take 17.1 gps as start point, convert the other distance into
+*   local pose.
+*
+*******************************************************************************/
+
+
+int main(int argc, char** argv) {
+
+return 0;
+}
